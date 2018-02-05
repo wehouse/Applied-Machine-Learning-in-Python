634ecbc9805a4f5675b61f7792edab1ca6f2b764
Daniel Yinanc <danielyinanc@users.noreply.github.com>
2018-02-04 22:15:57 -0500

    Soln2 First Problem Done

--- soln2.py
+++ soln2.py
@@ -32,18 +32,31 @@ def part1_scatter():
 def answer_one():
     from sklearn.linear_model import LinearRegression
     from sklearn.preprocessing import PolynomialFeatures
-    linreg = LinearRegression().fit(X_train.reshape(-1, 1), y_train.reshape(-1, 1))
-    ret_arr = np.zeros(0)
-    ret_arr.append(linreg.predict(np.linspace(0,10,100).reshape(-1, 1)))
+    linreg = LinearRegression().fit(X_train.reshape(-1,1), y_train.reshape(-1,1))
+    ret_arr = []
+    ret_arr.append(np.squeeze(linreg.predict(np.linspace(0,10,100).reshape(-1,1))))
     # Your code here
     poly3 = PolynomialFeatures(degree=3)
     X_poly3 = poly3.fit_transform(x.reshape(-1,1))
-    X_train_F3, X_test, y_train_F3, y_test = train_test_split(X_poly3, y,
-                                                   random_state = 0)
+    X_train_F3, X_test, y_train_F3, y_test = train_test_split(X_poly3, y,random_state = 0)
     linreg3 = LinearRegression().fit(X_train_F3, y_train_F3)
     
     ret_arr.append(linreg3.predict(poly3.transform(np.linspace(0,10,100).reshape(-1,1))))
+    
+    poly6 = PolynomialFeatures(degree=6)
+    X_poly6 = poly6.fit_transform(x.reshape(-1,1))
+    X_train_F6, X_test, y_train_F6, y_test = train_test_split(X_poly6, y,random_state = 0)
+    linreg6 = LinearRegression().fit(X_train_F6, y_train_F6)
+    
+    ret_arr.append(linreg6.predict(poly6.transform(np.linspace(0,10,100).reshape(-1,1))))
+    
+    poly9 = PolynomialFeatures(degree=9)
+    X_poly9 = poly9.fit_transform(x.reshape(-1,1))
+    X_train_F9, X_test, y_train_F9, y_test = train_test_split(X_poly9, y,random_state = 0)
+    linreg9 = LinearRegression().fit(X_train_F9, y_train_F9)
+    
+    ret_arr.append(linreg9.predict(poly9.transform(np.linspace(0,10,100).reshape(-1,1))))
 
-    return ret_arr
+    return np.array(ret_arr)
 
-answer_one()
+answer_one().shape
